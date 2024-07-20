1da5d1c1d270ce4b4aca5b9323fc110e8ea844b7
Radhitka <raditfatner47@gmail.com>
2024-07-20 11:58:02 +0700

    feat: add favorite and checkpint ayat post

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ import express from 'express';
 import { responseList, responseListDetail } from './response/index.js';
 
 import 'dotenv/config';
+import db from './database/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// For parsing application/json
+app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 const httpAxios = axios.create({
   baseURL: 'https://equran.id/api/v2/',
@@ -26,7 +33,7 @@ app.use(
   })
 );
 
-app.get('/surah', async function name(req, res) {
+app.get('/surah', async function (req, res) {
   const response = await httpAxios.get('/surat');
 
   const body = response?.data;
@@ -38,7 +45,7 @@ app.get('/surah', async function name(req, res) {
   });
 });
 
-app.get('/surah/:nomor', async function name(req, res) {
+app.get('/surah/:nomor', async function (req, res) {
   const nomor = req.params.nomor;
   const response = await httpAxios.get(`/surat/${nomor}`);
 
@@ -49,6 +56,72 @@ app.get('/surah/:nomor', async function name(req, res) {
     message: body.message,
     data: responseListDetail(body.data),
   });
+});
+
+app.post('/surah/favorite/:nomor', async function (req, res) {
+  const { nomor } = req.params;
+  const { ayah } = req.body;
+
+  try {
+    const sqlDetail =
+      'SELECT * FROM ayah_favorites WHERE user_id = ? and surah_id = ? and ayah_id = ? LIMIT 1';
+
+    const [resultsDetail] = await db.query(sqlDetail, [1, nomor, ayah]);
+
+    if (resultsDetail.length > 0) {
+      const sqlDelete = 'DELETE FROM ayah_favorites WHERE id = ?';
+
+      await db.query(sqlDelete, [resultsDetail[0].id]);
+    } else {
+      const sqlInsert =
+        'INSERT INTO ayah_favorites (user_id, surah_id, ayah_id) VALUES (?, ? , ?)';
+
+      await db.query(sqlInsert, [1, nomor, ayah]);
+    }
+
+    res.status(200).json({
+      code: 200,
+      message: 'Success',
+    });
+  } catch (err) {
+    res.status(500).json({
+      code: 500,
+      message: err.message,
+    });
+  }
+});
+
+app.post('/surah/checkpoints/:nomor', async function (req, res) {
+  const { nomor } = req.params;
+  const { ayah } = req.body;
+
+  try {
+    const sqlDetail =
+      'SELECT * FROM ayah_checkpoints WHERE user_id = ? and surah_id = ? and ayah_id = ? LIMIT 1';
+
+    const [resultsDetail] = await db.query(sqlDetail, [1, nomor, ayah]);
+
+    if (resultsDetail.length > 0) {
+      const sqlDelete = 'DELETE FROM ayah_checkpoints WHERE id = ?';
+
+      await db.query(sqlDelete, [resultsDetail[0].id]);
+    } else {
+      const sqlInsert =
+        'INSERT INTO ayah_checkpoints (user_id, surah_id, ayah_id) VALUES (?, ? , ?)';
+
+      await db.query(sqlInsert, [1, nomor, ayah]);
+    }
+
+    res.status(200).json({
+      code: 200,
+      message: 'Success',
+    });
+  } catch (err) {
+    res.status(500).json({
+      code: 500,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
